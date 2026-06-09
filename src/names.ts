72c893ca72d65b07b8f0b3c987e9627c9d833bd2
zigai <ziga.ivansek@gmail.com>
2026-06-09 22:30:13 +0200

    fix(src): convert dots to dashes in kebab-case names

--- src/names.ts
+++ src/names.ts
@@ -1,7 +1,7 @@
 export function toKebabCase(name: string): string {
     return name
         .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
-        .replace(/_/g, "-")
+        .replace(/[_.]/g, "-")
         .toLowerCase();
 }
 
