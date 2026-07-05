92b4138d56b4a5af6017f7dddcbbcb1be962e355
zigai <ziga.ivansek@gmail.com>
2026-07-05 17:48:16 +0200

    feat(pi): add command subpath export

---
+++ src/pi/command.ts
@@ -0,0 +1,8 @@
+export { defineTypedCommand } from "../command/definition.js";
+export { registerTypedCommand } from "./register.js";
+export type {
+    DefinedTypedCommand,
+    InferArguments,
+    TypedCommandDefinition,
+    TypedCommandHandle,
+} from "../types.js";
